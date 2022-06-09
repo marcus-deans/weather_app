244f431c7c6d2554edf78e27899364b4e86c39c4
Marcus Deans <marcus.deans@duke.edu>
2022-06-08 22:33:21 -0400

    building out API structure

---
+++ packages/open_meteo_api/lib/src/models/weather.dart
@@ -0,0 +1,25 @@
+enum WeatherState {
+  snow,
+  sleet,
+  hail,
+  thunderstorm,
+  heavyRain,
+  lightRain,
+  showers,
+  heavyCloud,
+  lightCloud,
+  clear,
+  unknown
+}
+
+enum WindDirectionCompas {
+  north,
+  northEast,
+  east,
+  southEast,
+  south,
+  southWest,
+  west,
+  northWest,
+  unknown
+}
